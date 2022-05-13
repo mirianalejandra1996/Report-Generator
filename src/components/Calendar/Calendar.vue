dea720239573d55873363b53102cc2d0499562ed
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2022-05-12 21:49:31 -0500

    implement calendar

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -1,13 +1,49 @@
 <template>
-  <h1>Calendar Vue</h1>
+  <datepicker v-model="picked" />
+</template>
+
+
+<script>
+import Datepicker from 'vue3-datepicker'
+
+export default {
+  name: "CalendarComponent",
+  components: {
+    Datepicker
+  },
+  data() {
+    return {
+      picked: new Date(),
+    }
+  }
+}
+
+</script>
+
+
+<!-- <template>
+    <Datepicker v-model="date"></Datepicker>
 </template>
 
 <script>
-export default {
-    name: "TableComponent",
-}
-</script>
+    import Datepicker from '@vuepic/vue-datepicker';
+    import '@vuepic/vue-datepicker/dist/main.css'
+    
+    export default {
+      name: "CalendarComponent",
+        components: { Datepicker },
+        data() {
+            return {
+                date: null,
+            };
+        }
+    }
+</script> -->
 
-<style>
 
-</style>
+
+
+
+
+
+ <!-- name: "CalendarComponent", -->
--- src/components/Calendar/CalendarProof.vue
+++ src/components/Calendar/CalendarProof.vue
@@ -16,7 +16,7 @@
 import moment from "moment"
 
 export default {
-  name: "TableComponent",
+  name: "TableProofComponent",
   data() {
     return {
       fecha: null,
